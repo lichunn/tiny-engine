c77832a9070afa0cc163fe7de62cd49b260fc780
lichunn <269031597@qq.com>
2024-12-09 18:21:07 -0800

    feat: 调整路由出码结构

--- packages/vue-generator/src/plugins/genRouterPlugin.js
+++ packages/vue-generator/src/plugins/genRouterPlugin.js
@@ -5,26 +5,69 @@ const defaultOption = {
   path: './src/router'
 }
 
-const parseSchema = (schema) => {
+// const parseSchema = (schema) => {
+//   const { pageSchema } = schema
+
+//   const routes = pageSchema.map(({ meta: { isHome = false, router = '' } = {}, fileName, path }) => ({
+//     filePath: `@/views${path ? `/${path}` : ''}/${fileName}.vue`,
+//     fileName,
+//     isHome,
+//     path: router?.startsWith?.('/') ? router : `/${router}`
+//   }))
+
+//   const hasRoot = routes.some(({ path }) => path === '/')
+
+//   if (!hasRoot && routes.length) {
+//     const { path: homePath } = routes.find(({ isHome }) => isHome) || { path: routes[0].path }
+
+//     routes.unshift({ path: '/', redirect: homePath })
+//   }
+
+//   return routes
+// }
+
+const convertToNestedRoutes = (schema) => {
   const { pageSchema } = schema
+  const result = []
 
-  const routes = pageSchema.map(({ meta: { isHome = false, router = '' } = {}, fileName, path }) => ({
-    filePath: `@/views${path ? `/${path}` : ''}/${fileName}.vue`,
-    fileName,
-    isHome,
-    path: router?.startsWith?.('/') ? router : `/${router}`
-  }))
+  pageSchema.forEach((item) => {
+    const parts = item.meta?.router?.split('/').filter(Boolean)
+    let curretnLevel = result
 
-  const hasRoot = routes.some(({ path }) => path === '/')
+    parts.forEach((part, index) => {
+      let found = false
 
-  if (!hasRoot && routes.length) {
-    const { path: homePath } = routes.find(({ isHome }) => isHome) || { path: routes[0].path }
+      for (let i = 0; i < curretnLevel.length; i++) {
+        if (curretnLevel[i].path === part) {
+          // 如果已经存在该路径部分，则进入下一层级
+          curretnLevel = curretnLevel[i].children
+          found = true
+          break
+        }
+      }
+      if (!found) {
+        // 如果不存在该路径部分，创建一个新节点
+        const newNode = {
+          path: part,
+          component: undefined,
+          children: []
+        }
+        // 如果路径是最后一步，则设置组件和属性
+        if (index === parts.length - 1) {
+          // if(path === '/')
+          newNode.component = `() => import('@/views${item.path ? `/${item.path}` : ''}/${item.fileName}.vue'`
+        }
 
-    routes.unshift({ path: '/', redirect: homePath })
-  }
+        curretnLevel.push(newNode)
+        curretnLevel = newNode.children
+      }
+    })
+  })
 
-  return routes
+  return result
 }
+
+// 示例路由数组
 
 function genRouterPlugin(options = {}) {
   const realOptions = mergeOptions(defaultOption, options)
@@ -40,34 +83,34 @@ function genRouterPlugin(options = {}) {
      * @returns
      */
     run(schema) {
-      const routesList = parseSchema(schema)
+      const routesList = convertToNestedRoutes(schema)
 
       // TODO: 支持 hash 模式、history 模式
       const importSnippet = "import { createRouter, createWebHashHistory } from 'vue-router'"
       const exportSnippet = `
-export default createRouter({
-  history: createWebHashHistory(),
-  routes
-})`
-      const routes = routesList.map(({ fileName, path, redirect, filePath }) => {
-        let pathAttr = `path: '${path}'`
-        let redirectAttr = ''
-        let componentAttr = ''
+      export default createRouter({
+        history: createWebHashHistory(),
+        routes
+      })`
+      //       const routes = routesList.map(({ fileName, path, redirect, filePath }) => {
+      //         let pathAttr = `path: '${path}'`
+      //         let redirectAttr = ''
+      //         let componentAttr = ''
 
-        if (redirect) {
-          redirectAttr = `redirect: '${redirect}'`
-        }
+      //         if (redirect) {
+      //           redirectAttr = `redirect: '${redirect}'`
+      //         }
 
-        if (fileName) {
-          componentAttr = `component: () => import('${filePath}')`
-        }
+      //         if (fileName) {
+      //           componentAttr = `component: () => import('${filePath}')`
+      //         }
 
-        const res = [pathAttr, redirectAttr, componentAttr].filter((item) => Boolean(item)).join(',')
+      // const res = [pathAttr, redirectAttr, componentAttr].filter((item) => Boolean(item)).join(',')
 
-        return `{${res}}`
-      })
+      // return `{${res}}`
+      //   })
 
-      const routeSnippets = `const routes = [${routes.join(',')}]`
+      const routeSnippets = `const routes = ${JSON.stringify(routesList, null, 2)}`
 
       const res = {
         fileType: 'js',
